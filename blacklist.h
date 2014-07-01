d039cd45e4915fc2cc17b5907442a0798858c327
Jeroen Nijhof <jeroen@jeroennijhof.nl>
2014-07-01 22:15:24 +0200

    Made the blacklist buffer a bit bigger

--- blacklist.h
+++ blacklist.h
@@ -6,7 +6,7 @@
  * blacklist.h: blacklist backend
  */
 
-#define BLACKLIST_MAXSIZE 1000
+#define BLACKLIST_MAXSIZE 10000
 
 extern int blacklist_load(const char *blacklist_file, char blacklist[BLACKLIST_MAXSIZE][URL_SIZE]);
 extern int blacklist_backend(char blacklist[BLACKLIST_MAXSIZE][URL_SIZE], char url[URL_SIZE], int debug);
